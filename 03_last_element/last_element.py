1824637e61e3ace435c6e7d7c74b5eee10b6138c
Sajid Jaber <sajidjaber@yahoo.com>
2024-01-26 21:39:27 -0600

    03 last element

--- 03_last_element/last_element.py
+++ 03_last_element/last_element.py
@@ -1,9 +1,18 @@
-def last_element(lst):
-    """Return last item in list (None if list is empty.
+def last_element(list):
+    if len(list) <= 0:
+        print("True")
+    else:
+        print(list.pop())
+    
+    
+    
+"""Return last item in list (None if list is empty.
     
         >>> last_element([1, 2, 3])
         3
         
         >>> last_element([]) is None
         True
-    """
+"""
+last_element([1,2,3])
+last_element([])
