e74da4a41f24a5cc7aab4c6623ebd5722dcb4495
Sajid Jaber <sajidjaber@yahoo.com>
2024-01-26 21:57:20 -0600

    05 string reverse

--- 05_reverse_string/reverse_string.py
+++ 05_reverse_string/reverse_string.py
@@ -1,4 +1,12 @@
 def reverse_string(phrase):
+    lists = list(phrase)
+    lists.reverse()
+    str = ""
+    for li in lists:
+        str += li
+    
+    print(str)
+    
     """Reverse string,
 
         >>> reverse_string('awesome')
@@ -7,3 +15,5 @@ def reverse_string(phrase):
         >>> reverse_string('sauce')
         'ecuas'
     """
+reverse_string('awesome')
+reverse_string('sauce')
