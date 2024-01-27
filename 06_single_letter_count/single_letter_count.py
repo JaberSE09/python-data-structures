4d4157511484d94a4fbe84970c4e98af1a3c41ca
Sajid Jaber <sajidjaber@yahoo.com>
2024-01-26 22:02:57 -0600

    06 single letter count

--- 06_single_letter_count/single_letter_count.py
+++ 06_single_letter_count/single_letter_count.py
@@ -1,4 +1,12 @@
 def single_letter_count(word, letter):
+    
+    count = 0
+    for char in word:
+        if char.lower() == letter.lower():
+            count += 1
+    print(count)
+    
+    
     """How many times does letter appear in word (case-insensitively)?
     
         >>> single_letter_count('Hello World', 'h')
@@ -10,3 +18,7 @@ def single_letter_count(word, letter):
         >>> single_letter_count("Hello World", 'l')
         3
     """
+
+single_letter_count('Hello World', 'h')
+single_letter_count('Hello World', 'z')
+single_letter_count("Hello World", 'l')
