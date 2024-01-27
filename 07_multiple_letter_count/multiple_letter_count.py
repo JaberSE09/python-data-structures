f79d55444f3279f99d4e760b1221bc04b38ca949
Sajid Jaber <sajidjaber@yahoo.com>
2024-01-26 22:11:22 -0600

    07 multiple letter count

--- 07_multiple_letter_count/multiple_letter_count.py
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -1,4 +1,12 @@
 def multiple_letter_count(phrase):
+    letter_counts = {}
+    for letter in phrase:
+        if letter not in letter_counts:
+            letter_counts[letter] = 0
+        letter_counts[letter] += 1  
+    print(letter_counts)
+
+
     """Return dict of {ltr: frequency} from phrase.
 
         >>> multiple_letter_count('yay')
@@ -7,3 +15,7 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    
+multiple_letter_count('yay')
+multiple_letter_count('Yay')
+    
