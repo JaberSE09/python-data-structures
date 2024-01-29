0cd41fad0d0f4a7570124378dd9a4a2ea884c065
Sajid Jaber <sajidjaber@yahoo.com>
2024-01-29 09:03:20 -0600

    11 done

--- 11_flip_case/flip_case.py
+++ 11_flip_case/flip_case.py
@@ -1,4 +1,15 @@
 def flip_case(phrase, to_swap):
+    to_swap = to_swap.lower()
+    out = ""
+
+    for ltr in phrase:
+        if ltr.lower() == to_swap:
+            ltr = ltr.swapcase()
+        out += ltr
+
+    return out
+
+
     """Flip [to_swap] case each time it appears in phrase.
 
         >>> flip_case('Aaaahhh', 'a')
@@ -11,3 +22,6 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+print( flip_case('Aaaahhh', 'a'))
+print( flip_case('Aaaahhh', 'A'))
+print(flip_case('Aaaahhh', 'h'))
