9663887633df285138e44991a04f6f49344cba0f
Sajid Jaber <sajidjaber@yahoo.com>
2024-01-29 09:25:07 -0600

    15 done

--- 15_intersection/intersection.py
+++ 15_intersection/intersection.py
@@ -1,4 +1,5 @@
 def intersection(l1, l2):
+    return print(list(set(l1) & set(l2)))
     """Return intersection of two lists as a new list::
     
         >>> intersection([1, 2, 3], [2, 3, 4])
@@ -13,3 +14,7 @@ def intersection(l1, l2):
         >>> intersection([1, 2, 3], [4, 5, 6])
         []
     """
+intersection([1, 2, 3], [2, 3, 4])
+intersection([1, 2, 3], [1, 2, 3, 4])
+intersection([1, 2, 3], [3, 4])
+intersection([1, 2, 3], [4, 5, 6])
