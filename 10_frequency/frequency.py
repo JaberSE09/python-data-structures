f1fded61f3463c6aa251eaee2ebdb337da365dad
Sajid Jaber <sajidjaber@yahoo.com>
2024-01-29 09:00:35 -0600

    10 done

--- 10_frequency/frequency.py
+++ 10_frequency/frequency.py
@@ -1,4 +1,5 @@
 def frequency(lst, search_term):
+    return lst.count(search_term)
     """Return frequency of term in lst.
     
         >>> frequency([1, 4, 3, 4, 4], 4)
@@ -7,3 +8,6 @@ def frequency(lst, search_term):
         >>> frequency([1, 4, 3], 7)
         0
     """
+    
+print(frequency([1, 4, 3, 4, 4], 4))
+print(frequency([1, 4, 3], 7))
