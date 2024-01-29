e4d6cc2aa21e9370979e719ffb653842b87f8076
Sajid Jaber <sajidjaber@yahoo.com>
2024-01-29 10:00:52 -0600

    17 done

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -1,4 +1,19 @@
 def mode(nums):
+        # Make frequency counter of num:freq
+    counts = {}
+
+    for num in nums:
+        counts[num] = counts.get(num, 0) + 1
+
+    # find the highest value (the most frequent number)
+
+    max_value = max(counts.values())
+
+    # now we need to see at which index the highest value is at
+
+    for (num, freq) in counts.items():
+        if freq == max_value:
+            return print(num)
     """Return most-common number in list.
 
     For this function, there will always be a single-most-common value;
@@ -11,3 +26,5 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+mode([1, 2, 1])
+mode([2, 2, 3, 3, 2])
