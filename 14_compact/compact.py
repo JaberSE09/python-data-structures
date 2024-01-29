2df5b378089368d18bb18b432fd172b9d0a70ef3
Sajid Jaber <sajidjaber@yahoo.com>
2024-01-29 09:18:15 -0600

    14 done

--- 14_compact/compact.py
+++ 14_compact/compact.py
@@ -1,6 +1,9 @@
 def compact(lst):
+    return [val for val in lst if val]
+
     """Return a copy of lst with non-true elements removed.
 
         >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
         [1, 2, 'All done']
     """
+print(compact([0, 1, 2, '', [], False, (), None, 'All done']))
