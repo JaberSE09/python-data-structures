b06831b8a4e5dc8a598871c4db48040a372a0ee4
Sajid Jaber <sajidjaber@yahoo.com>
2024-02-01 11:10:22 -0600

    24 done

--- 24_remove_every_other/remove_every_other.py
+++ 24_remove_every_other/remove_every_other.py
@@ -1,4 +1,5 @@
 def remove_every_other(lst):
+    return print([val for i, val in enumerate(lst) if i %2 ==0])
     """Return a new list of other item.
 
         >>> lst = [1, 2, 3, 4, 5]
@@ -11,3 +12,6 @@ def remove_every_other(lst):
         >>> lst
         [1, 2, 3, 4, 5]
     """
+
+lst = [1, 2, 3, 4, 5]
+remove_every_other(lst)
