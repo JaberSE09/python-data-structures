5499da7f704a81ea2dc1c063de82859bdff6f794
Sajid Jaber <sajidjaber@yahoo.com>
2024-02-01 11:33:23 -0600

    278 done

--- 28_find_factors/find_factors.py
+++ 28_find_factors/find_factors.py
@@ -1,4 +1,11 @@
 def find_factors(num):
+    factors = []
+    n=1
+    while(n <= num): 
+        if num % n == 0:
+            factors.append(n)
+        n+=1
+    return print(factors)
     """Find factors of num, in increasing order.
 
     >>> find_factors(10)
@@ -13,3 +20,5 @@ def find_factors(num):
     >>> find_factors(321421)
     [1, 293, 1097, 321421]
     """
+    
+find_factors(10)
